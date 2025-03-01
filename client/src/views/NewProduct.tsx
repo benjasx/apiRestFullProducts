d7339017badac647474a38d6f207ff2ba2158d5f
benjasx <benjasanchez175@gmail.com>
2025-02-28 20:13:55 -0700

    Added styles

--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
 
 
 export default function NewProduct() {
   return (
-    <div>NewProduct</div>
+    <>
+            <div className="flex justify-between">
+                <h2 className="text-4xl font-black text-slate-500">Registrar productos</h2>
+                <Link
+                    to="/"
+                    className="bg-indigo-600 rounded-md p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500"
+                >
+                    Ver productos
+                </Link>
+            </div>
+        </>
   )
 }
